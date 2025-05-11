2a7d43f899fb62fdf65cd8ad94d3d6de42818867
Zombiedenden <artdenden@gmail.com>
2025-05-11 15:39:36 +0200

    Add server configuration to Vite setup

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		port: 51732, // runs with localhost:51732
+		host: true, // allows access from other devices on the network
+		strictPort: true // exits if port is already taken (no fallback)
+	}
 });
