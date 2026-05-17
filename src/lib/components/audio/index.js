32ffb69997fdc8a4008596e162c4305f3c4689df
Pablo Alvarado <pibulus@gmail.com>
2026-05-17 14:00:43 +1000

    chore: remove unused audio barrel

--- src/lib/components/audio/index.js
+++
@@ -1,4 +0,0 @@
-export { default as AudioToText } from './AudioToText.svelte';
-export { default as AudioVisualizer } from './AudioVisualizer.svelte';
-export { default as RecordButtonWithTimer } from './RecordButtonWithTimer.svelte';
-export { default as TranscriptDisplay } from './TranscriptDisplay.svelte';
