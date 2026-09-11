60b74c4627d9020bfc4df9cc3463255aa5094ae9
Pablo Alvarado <pibulus@gmail.com>
2026-09-11 06:44:04 -0600

    feat: 🇲🇽 add bidirectional hreflang tags across all layout routes

--- src/lib/components/layout/Seo.svelte
+++ src/lib/components/layout/Seo.svelte
@@ -43,8 +43,9 @@
 <svelte:head>
 	<title>{title}</title>
 	<meta name="description" content={description} />
-	<meta name="robots" content={robots} />
 	<link rel="canonical" href={canonicalUrl} />
+	<link rel="alternate" hreflang="en" href="https://talktype.app" />
+	<link rel="alternate" hreflang="es" href="https://talktype.app/es" />
 
 	<meta property="og:type" content={type} />
 	<meta property="og:url" content={canonicalUrl} />
